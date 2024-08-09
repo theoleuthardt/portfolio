9284f6d4b8c5ffc0bcf78a7e58c9c23261adab19
theoleuthardt <theoleuthardt12@gmail.com>
2024-08-09 21:56:44 +0200

    Navbar small display burger menu fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -54,7 +54,24 @@ export const Navbar = () => {
           <div
             className={`${!toggle ? "hidden" : "flex"} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 
             min-w-[140px] z-10 rounded-xl`}
-          ></div>
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } font-poppins font-medium text-[16px] hover:text-white cursor-pointer`}
+                  onClick={() => {
+                    setActive(link.title);
+                    setToggle(!toggle);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
