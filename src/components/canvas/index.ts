083f17f1a8b98c45d23c75a832125dc689abc8c1
theoleuthardt <theoleuthardt12@gmail.com>
2024-08-19 19:56:02 +0200

    fix: canvas components convert to typescript files

---
+++ src/components/canvas/index.ts
@@ -0,0 +1,6 @@
+import EarthCanvas from "./Earth.tsx";
+import BallCanvas from "./Ball.tsx";
+import ComputersCanvas from "./Computers.tsx";
+import StarsCanvas from "./Stars.tsx";
+
+export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
