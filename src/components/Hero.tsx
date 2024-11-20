f4cead9bb6f186cf2da1c3ee3cc060b2b7dd154e
theoleuthardt <theoleuthardt12@gmail.com>
2024-11-20 18:49:29 +0100

    Hero updated messages

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,21 +5,15 @@ export const Hero = () => {
     <div
       className="lg:w-[48rem] md:w-[40rem] sm:w-[30rem] w-[20rem]
       h-auto lg:text-8xl md:text-7xl sm:text-6xl text-6xl text-white text-center 
-      font-bold cursor-pointer"
+      font-bold tracking-wide"
     >
       <Typewriter
         onInit={(typewriter) => {
           typewriter
-            .typeString("Welcome!")
-            .pauseFor(1000)
+            .typeString("Hii! I'm Theo.")
+            .pauseFor(900)
             .deleteAll()
-            .typeString("I'm Theo.")
-            .pauseFor(500)
-            .deleteAll()
-            .typeString("I'm a cs student.")
-            .pauseFor(500)
-            .deleteAll()
-            .typeString("Feel free to look around!")
+            .typeString("Welcome to my Portfolio!")
             .start();
         }}
       />
